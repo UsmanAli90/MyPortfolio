2d8babdf573aca237995c258dba5c573e559de03
UsmanAli90 <usmanali04768@gmail.com>
2024-11-13 16:26:48 +0500

    Updated About Page

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
+import { DiRuby } from "react-icons/di";
+import { SiRubyonrails } from "react-icons/si"; 
+import { SiExpress } from "react-icons/si"; 
+import { SiDjango } from "react-icons/si"; 
 import {
   DiJavascript1,
   DiReact,
@@ -8,7 +12,7 @@ import {
   DiMongodb,
   DiPython,
   DiGit,
-  DiJava,
+  DiJava
 } from "react-icons/di";
 import {
   SiRedis,
@@ -22,47 +26,59 @@ import { TbBrandGolang } from "react-icons/tb";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
+       <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
+        <SiExpress />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
       {/* <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
+        <DiJavascript1 />
       </Col> */}
       {/* <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <TbBrandGolang />
       </Col> */}
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiSolidity />
+      </Col> */}
+      
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col> */}
+     
       {/* <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col> */}
       {/* <Col xs={4} md={2} className="tech-icons">
         <SiRedis />
       </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
-      </Col> */}
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRubyonrails />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDjango />
+      </Col>
+     
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
       </Col>
     </Row>
   );
